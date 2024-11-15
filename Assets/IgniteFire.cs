c0b64d7e9a2145f7731819eca3b23e2ebc3cc95b
FIH-COM23a112 <user@FIH-COM23a112.fsu.edu>
2024-11-15 16:27:56 -0500

    Final_Day

--- Assets/IgniteFire.cs
+++ Assets/IgniteFire.cs
@@ -5,19 +5,29 @@ using UnityEngine;
 public class IgniteFire : MonoBehaviour
 {
     public ParticleSystem Fire_01;
-    public ParticleSystem Fire_02; 
+    public ParticleSystem Fire_02;
     public ParticleSystem Fire_03;
 
     private void OnTriggerEnter(Collider other)
     {
+        // Check if the object entering the trigger has the "Skull" tag
         if (other.CompareTag("Skull"))
         {
-            Fire_01.Play() ; 
-            Fire_02.Play() ;   
-            Fire_03.Play() ;
+            Debug.Log("Skull placed on the stick, enabling fire...");
 
-            Debug.Log("Skull placed on the stick, fire ignited");
-
+            // Ensure fire is enabled and playing
+            EnableAndPlayFire(Fire_01);
+            EnableAndPlayFire(Fire_02);
+            EnableAndPlayFire(Fire_03);
         }
+    }
+
+    private void EnableAndPlayFire(ParticleSystem fireSystem)
+    {
+        if (!fireSystem.gameObject.activeSelf)
+        {
+            fireSystem.gameObject.SetActive(true); // Enable the fire object
+        }
+        fireSystem.Play(); // Play the fire particle system
     }
 }
